bcf1df1b055ae92d618558eb52783993a8a3d8e0
teranixbq <hanieffathulb03@gmail.com>
2023-10-29 23:35:28 +0700

    add: average rating and refactor model

--- internal/features/user/entity/entityCore.go
+++ internal/features/user/entity/entityCore.go
@@ -1,27 +1,24 @@
 package entity
 
 import (
+	_feedback "terhandle/internal/features/feedback/entity"
 	"time"
-	_feedback"terhandle/internal/features/feedback/entity"
 )
 
-
 type Core struct {
-	Id          int
-	Role         string 
-	Profesi      string
-	Nama         string 
-	NIK          int   
-	Alamat       string
-	Longitude    float64 
-	Latitude     float64
-	No_telp      int   
-	Email        string
-	Password     string 
-	Status       string   
-	Feedback     []_feedback.CoreFeedback 
-	CreatedAt    time.Time 
-	UpdatedAt    time.Time 
+	Id        int
+	Role      string
+	Profesi   string
+	Nama      string
+	NIK       int
+	Alamat    string
+	Longitude float64
+	Latitude  float64
+	No_telp   int
+	Email     string
+	Password  string
+	Status    string
+	Feedback  []_feedback.CoreFeedback
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
-
-
